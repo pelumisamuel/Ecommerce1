41070963c3525844c8d18a080291056ce5cdb01d
duropel <samueladigun@gmail.com>
2021-03-27 13:41:54 -0700

    homescreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import { Container } from "react-bootstrap";
 
 const App= () => {
   return (
     <>
-      <h1>Welcome to proshop</h1>
+      <Header/>
+
+      <main className = 'py-3'>
+        <Container>
+        <h1>Welcome to proshop</h1>
+        </Container>
+        </main>
+
+      <Footer/>
+     
     </>
   );
 }
