3dec9afc195a53d69aed8dbf5a95d3909f3b8215
duropel <samueladigun@gmail.com>
2021-04-01 15:47:21 -0700

    mongo and compass setup

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,34 @@
-const express = require('express')
-const products = require('./Data/products')
-const dotenv = require('dotenv')
+// const express = require('express')
+// const products = require('./Data/products')
+// const dotenv = require('dotenv')
 
+import express from 'express'
+import products from './Data/products.js'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 const app = express()
 
-app.get('/',(req, res) => {
-    res.send('Api is running...')
+app.get('/', (req, res) => {
+  res.send('Api is running...')
 })
 
-app.get('/api/products/',(req, res) => {
-    res.json(products)
+app.get('/api/products/', (req, res) => {
+  res.json(products)
 })
 
-app.get('/api/products/:id',(req, res) => {
-   const product = products.find((p) => p._id === req.params.id) 
-   
-    res.json(product)
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+
+  res.json(product)
 })
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`server is running in ${process.env.NODE_ENV} mode on  port ${PORT}`))
+app.listen(
+  PORT,
+  console.log(
+    `server is running in ${process.env.NODE_ENV} mode on  port ${PORT}`
+  )
+)
