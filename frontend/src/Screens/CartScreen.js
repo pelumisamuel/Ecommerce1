364a3a4960328851445481767955baa726f2859e
duropel <samueladigun@gmail.com>
2021-04-09 18:05:44 -0700

    adding cart functionality

--- frontend/src/Screens/CartScreen.js
+++ frontend/src/Screens/CartScreen.js
@@ -1,4 +1,28 @@
-const CartScreen = () => {
+import { useEffect } from 'react'
+import { link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form } from 'react-bootstrap'
+import Message from '../Components/Message'
+import { addToCart } from '../Actions/cartActions'
+import Loader from '../Components/Loader'
+
+const CartScreen = ({ match, location, history }) => {
+  const productID = match.params.id
+
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+  const dispatch = useDispatch()
+
+  const cart = useSelector((state) => state.cart)
+
+  console.log(cart.cartItems)
+
+  useEffect(() => {
+    if (productID) {
+      dispatch(addToCart(productID, qty))
+    }
+  }, [useEffect, productID, qty])
+
   return <div>Cart</div>
 }
 
