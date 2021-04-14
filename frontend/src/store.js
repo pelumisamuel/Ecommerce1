e4d43dd6286c41331111c1ca60ce66c339c0e486
duropel <samueladigun@gmail.com>
2021-04-13 17:11:18 -0700

    user actions and reducer created

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,19 +6,25 @@ import {
   productDetailsReducer,
 } from './Reducers/productReducers'
 import { cartReducer } from './Reducers/cartReducers'
+import { userLoginReducer } from './Reducers/userReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  user: userLoginReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
+const userFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
 
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  user: { userInfo: userFromStorage },
 }
 
 const middleware = [thunk]
